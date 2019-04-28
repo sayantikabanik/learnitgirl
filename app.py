cd9683ceedc63243a5d90f61116bfad691b93bac
Devanshi Tank <devanshi07.tank@gmail.com>
2019-04-28 09:12:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask, render_template
+from data import Diseases
+
 app=Flask(__name__)
+
+Diseases=Diseases()
 @app.route('/')
+
 def index():
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/diseases')
+def diseases():
+    return render_template('diseases.html', diseases=Diseases)
+
+@app.route('/disease/<string:id>')
+def disease(id):
+    return render_template('disease.html', id=id)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,30 @@
+def Diseases():
+diseases
+[
+{
+'id':1,
+'name':'Disease One',
+'symptoms':'gfojgofkoewds',
+'precaution':'fdgtrhrfdg',
+'cure':'fgdfgdf',
+},
+
+{
+'id':2,
+'name':'Disease Two',
+'symptoms':'gfojgofkoewds',
+'precaution':'fdgtrhrfdg',
+'cure':'fgdfgdf',
+
+},
+
+{
+'id':3,
+'name':'Disease Three',
+'symptoms':'gfojgofkoewds',
+'precaution':'fdgtrhrfdg',
+'cure':'fgdfgdf',
+}
+
+]
+return diseases
